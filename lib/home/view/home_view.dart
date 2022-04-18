f7fcf81df2bc036c345a4609f693fc9d2f8a689f
Ayush783 <ayushc45xxx@gmail.com>
2022-04-19 00:27:00 +0530

    home feature integrated

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../bloc/home_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -7,8 +11,105 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        appBar: AppBar(
+          title: Text(
+            'Current Users',
+            style: GoogleFonts.juliusSansOne(fontSize: 20),
+          ),
+          backgroundColor: const Color(0xffFF5470),
+        ),
+        body: BlocProvider(
+          create: (context) => HomeBloc()..add(LoadUsers()),
+          child: const HomeViewListener(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: const Color(0xffFF5470),
+          child: const Icon(
+            Icons.add,
+            size: 32,
+            color: Colors.white,
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class HomeViewListener extends StatelessWidget {
+  const HomeViewListener({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<HomeBloc, HomeState>(
+      listener: (context, state) {},
+      child: const HomeViewBody(),
+    );
+  }
+}
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        if (state.isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Color(0xffFF5470),
+            ),
+          );
+        } else {
+          if (state.hasError) {
+            return Center(
+              child: Text(
+                'An error occoured',
+                style: GoogleFonts.juliusSansOne(fontSize: 16),
+              ),
+            );
+          } else {
+            final users = state.users;
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView.builder(
+                itemBuilder: (context, index) => Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      users[index].username!,
+                      style: GoogleFonts.poppins(fontSize: 16),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.edit,
+                            color: Colors.green,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                itemCount: users.length,
+              ),
+            );
+          }
+        }
+      },
     );
   }
 }
