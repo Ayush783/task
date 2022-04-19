9ab562b7dfbefdfd5ed3467a1559ac9fd837f5ca
Ayush783 <ayushc45xxx@gmail.com>
2022-04-19 12:40:08 +0530

    working on manage users

---
+++ lib/manage_users/view/manage_users_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ManageView extends StatelessWidget {
+  const ManageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
